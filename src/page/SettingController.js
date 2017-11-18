93e7996b4a67acc7eae270b0423481ba071f09c6
Ivan <yifanb@gmail.com>
2017-11-18 20:29:41 +0800

    Add settings UI

--- src/page/SettingController.js
+++ src/page/SettingController.js
@@ -7,21 +7,76 @@ import {
     ScrollView,
     Dimensions,
 } from 'react-native'
+
+import { List, ListItem } from 'react-native-elements'
+
 import HeaderTitle from '../component/HeaderTitle'
 
-var contentHeight = Dimensions.get('window').height-22;
+var contentHeight = Dimensions.get('window').height;
 
 class SettingController extends Component {
-    static navigationOptions  ={
-        header: null,
-    };
+    static navigationOptions = ({navigation}) => ({
+        headerTitle: '设置' ,
+        headerStyle:{
+            backgroundColor: '#3a4172',
+        },
+        headerTitleStyle: {
+            color: '#ffffff',
+        },
+        headerBackTitleStyle:{
+            color: '#ffffff',
+        }
+    });
+
+
     render() {
+        const list = [
+            {
+                title: '基准货币',
+                icon: 'av-timer'
+            },
+            {
+                title: '关于',
+                icon: 'flight-takeoff'
+            },
+        ];
         return (
             <View>
-                <StatusBar barStyle="light-content"/>
-                <HeaderTitle title="设置" />
-                <ScrollView style={{height: contentHeight}}>
+                <View style={{
+                    alignItems: 'center',
+                    marginTop: 50,
+                }}>
+                    <Text style={{
+                        fontSize: 20,
 
+                    }}>CoinOnline</Text>
+                    <Text style={{
+                        color: '#666666',
+                    }}>您的数字货币小助手</Text>
+
+                </View>
+                <ScrollView style={{height: contentHeight}}>
+                    <List>
+                        <ListItem
+                            style ={ styles.list}
+                            title= "货币基准"
+                            leftIcon={{name: 'public'}}
+                        />
+                        <ListItem
+                            switchButton
+                            hideChevron
+                            onSwitch={() => {}}
+                            style ={ styles.list}
+                            title= "持久模式"
+                            leftIcon={{name: 'sd-card'}}
+                        />
+                        <ListItem
+                            style ={ styles.list}
+                            title= "关于"
+                            leftIcon={{name: 'favorite'}}
+                        />
+
+                    </List>
                 </ScrollView>
 
             </View>
@@ -29,4 +84,9 @@ class SettingController extends Component {
     }
 }
 
+var styles = StyleSheet.create({
+    list:{
+    }
+
+});
 export default SettingController
